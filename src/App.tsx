49402a4845861b737d3804375dbed2e055f66728
Ehtisham <ehtishamsattar888@gmail.com>
2023-11-17 22:29:47 +0500

    framer-motion

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import './App.css'
+
+function App() {
+  
+
+  return (
+    <>
+      <div>
+        Hey ... this is a React app using Vite ⚡️
+      </div>
+    </>
+  )
+}
+
+export default App
